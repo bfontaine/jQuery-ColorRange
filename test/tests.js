a17245d5286f14366bd3c1156730cc5dd285fb0a
Baptiste Fontaine <batifon@yahoo.fr>
2013-03-18 22:13:07 +0100

    basic code added.

--- test/tests.js
+++ test/tests.js
@@ -8,9 +8,32 @@
 
         });
 
-
     });
 
+    describe( '$().colorRange method', function() {
+
+        it( 'should return itself', function () {
+
+            var d = $( '<div/>' );
+
+            expect( d.colorRange() ).to.deep.equal( d );
+            expect( d.colorRange({}) ).to.deep.equal( d );
+
+        });
+
+        it( 'should not modify its color '
+          + 'if there\'s only one element in the selection', function() {
+
+            var d     = $( '<div/>' ).css( 'background-color', '#fff' ),
+                color = d.css( 'background-color' );
+
+            d.colorRange();
+
+            expect( d.css( 'background-color' ) ).to.equal( color );
+
+        });
+
+    });
 
 
 })( jQuery );
